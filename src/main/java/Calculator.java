1b4f299fe610ae3144454c42187186e7b35ad966
Damian Bernat <damianbernat@wp.pl>
2021-11-01 12:39:07 +0100

    Second Commit

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -2,8 +2,15 @@ public class Calculator {
     public static int add(int a, int b) {
         return a + b;
     }
+
+    public static int sub(int a, int b) {
+        return a - b;
+    }
     public static void main(String[] args) {
+        int a = 8;
+        int b = 3;
         Calculator calculator = new Calculator();
-        calculator.add(5, 3);
+        System.out.println(a + " + " + b + " equals " + calculator.add(a, b));
+        System.out.println(a + " - " + b + " equals " + calculator.sub(a, b));
     }
 }
